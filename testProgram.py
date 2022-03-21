50b4cbf5cd1d7270859bdf625b1c09ec87c649e5
pyth0n1c <87383215+pyth0n1c@users.noreply.github.com>
2022-03-21 13:21:07 -0700

    Commit a sample python file and test file to test the matrix functionality.

---
+++ testProgram.py
@@ -0,0 +1,14 @@
+import datetime
+import platform
+
+
+print("Platform.system : %s"%(platform.system()))
+print("Platform.release: %s"%(platform.release()))
+
+try:
+    print(f"Hello from an F-String at {datetime.datetime.now().isoformat()}")
+except Exception as e:
+    print("There was an error using F-Strings, they are not supported in Python2.7.")
+    sys.exit(1)
+
+sys.exit(0)
